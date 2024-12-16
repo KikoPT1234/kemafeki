2b304469a7bafa27612ab1097cedf9b7bf9507c5
Francisco Antunes Carriço <F.M.AntunesCarrico@student.tudelft.nl>
2024-12-16 19:46:53 +0100

    revamp

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
+// $(() => {
+//     $(".cloud-left, .cloud-right").each((index, element) => {
+//         const cloud = $(element);
+//         const cloudNumber = parseInt(
+//             cloud.attr("class").match(/cloud-([1-7])/)[1]
+//         );
+//         // const isLeft = cloud.hasClass("cloud-left");
+
+//         setScroll(cloud, cloudNumber);
+
+//         $(window).on("scroll", () => {
+//             setScroll(cloud, cloudNumber);
+//         });
+
+//         // setScroll(cloud, cloudNumber)
+//     });
+
+//     setScroll($("#title"), 1.5);
+//     setScroll($("#mountain"), 0.5, 0);
+
+//     $(window).on("scroll", () => {
+//         setScroll($("#title"), 1.5);
+//         setScroll($("#mountain"), 0.5, 0);
+//     });
+// });
+
 $(() => {
-    $(".cloud-left, .cloud-right").each((index, element) => {
-        const cloud = $(element);
-        const cloudNumber = parseInt(
-            cloud.attr("class").match(/cloud-([1-7])/)[1]
-        );
-        // const isLeft = cloud.hasClass("cloud-left");
+    const mountain = $("#mountain");
+    const cloudsBack = $("#clouds-back");
+    const cloudsFront = $("#clouds-front");
+    const title = $("#title");
 
-        setScroll(cloud, cloudNumber);
-
-        $(window).on("scroll", () => {
-            setScroll(cloud, cloudNumber);
-        });
-
-        // setScroll(cloud, cloudNumber)
-    });
-
-    setScroll($("#title"), 1.5);
-    setScroll($("#mountain"), 0.5, 0);
+    setScrolls();
 
     $(window).on("scroll", () => {
-        setScroll($("#title"), 1.5);
-        setScroll($("#mountain"), 0.5, 0);
+        setScrolls();
     });
+
+    function setScrolls() {
+        setScroll(mountain, 1);
+        setScroll(cloudsBack, 1.2);
+        // setScroll(cloudsFront, 3);
+        setScroll(title, 1.5);
+    }
 });
 
 function setScroll(element, factor) {
@@ -31,10 +51,4 @@ function setScroll(element, factor) {
 
     // if (factor < 6)
     element.css("bottom", -scrollTop / Math.exp(factor / 2) + "px");
-}
-
-function setScrollTop(element, factor) {
-    const scrollTop = $(window).scrollTop();
-    // if (factor < 6)
-    element.css("top", scrollTop / Math.exp(factor * 0.5) + "px");
 }
